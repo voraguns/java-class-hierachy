3d5f19c9f1b771308d0878874ee02071a1318094
Voragun <voragun2543@gmail.com>
2022-10-20 21:18:29 +0700

    nested class

--- code/Start.java
+++ code/Start.java
@@ -1,27 +1,25 @@
 class Start {
     public static void main(String[] args) {
-        MyNumber m = new MyNumber();
-        
-        System.out.println(m);
-        System.out.println(m.intValue());
+       A.B.C w = new A.B.C();
+       w.showDetail();
     }
 }
 
-sealed class Pool permits Student, Staff { }
-
-final class Student extends Pool { }
-non-sealed class Staff extends Pool { }
-
-class NationalTeam extends Staff { }
-
-
-sealed class Product permits Online, Offline { }
-final class Online extends Product { }
-final class Offline extends Product { }
-
-class MyNumber extends Number {
-    public int intValue() { return 0; }
-    public long longValue() { return 0; }
-    public float floatValue() { return 0; }
-    public double doubleValue() { return 0; }
+class A {
+    static class B {
+        static class C {
+            void showDetail() {
+                System.out.println("ABC");
+            }
+        }
+    }
+    void test() {
+        M m = new M();
+    }
+    class M { }
+    static class Z { }
+    class Z { }
+    interface I { }
+    record R() { }
+    enum E { }
 }
