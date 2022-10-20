6946ce039190f51819838a51fc79dfe08c6d6833
Voragun <voragun2543@gmail.com>
2022-10-20 19:32:59 +0700

    try to find the super class of these data type

--- code/Start.java
+++ code/Start.java
@@ -1,5 +1,15 @@
 class Start {
     public static void main(String[] args) {
-        Character c;
+        MyNumber m = new MyNumber();
+        
+        System.out.println(m);
+        System.out.println(m.intValue());
     }
+}
+
+class MyNumber extends Number {
+    public int intValue() { return 0; }
+    public long longValue() { return 0; }
+    public float floatValue() { return 0; }
+    public double doubleValue() { return 0; }
 }
